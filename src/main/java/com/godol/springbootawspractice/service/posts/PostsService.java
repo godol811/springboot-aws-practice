8a6843eff1bf16a3eec85c408fbf03788bae4379
Jongchan Ko <godol811@naver.com>
2022-09-18 16:52:01 +0900

    2022 09 18 EC2 Build test 중 오류 부분 삭제 (안쓰는 package 삭제)

--- src/main/java/com/godol/springbootawspractice/service/posts/PostsService.java
+++ src/main/java/com/godol/springbootawspractice/service/posts/PostsService.java
@@ -6,7 +6,6 @@ import com.godol.springbootawspractice.web.dto.PostsListResponseDto;
 import com.godol.springbootawspractice.web.dto.PostsResponseDto;
 import com.godol.springbootawspractice.web.dto.PostsSaveRequestDto;
 import com.godol.springbootawspractice.web.dto.PostsUpdateRequestDto;
-import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
